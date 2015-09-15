3863994baa9551fd88293ad8d1fddb4eae69162f
noklin <noklin.main@mail.com>
2015-09-15 19:02:09 +0300

    change params

--- server_run.bat
+++ server_run.bat
@@ -1,4 +1,4 @@
 @ECHO OFF  
 CLS 
-java -Dfile.encoding=cp866 -jar BroBoxServer\dist\BroBoxServer.jar 192.168.1.3 8765
+java -Dfile.encoding=cp866 -jar BroBoxServer\dist\BroBoxServer.jar localhost 8765
 pause
